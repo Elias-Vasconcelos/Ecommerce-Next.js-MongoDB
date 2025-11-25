dba6d2c9a4139746f57e05e079317a7c4eaf3579
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2025-11-25 15:10:35 -0300

    Continuaçao do desenvolvimento dos cards de produto

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { ProductType } from "./Types";
 import Image from "next/image";
+import { ShoppingCart } from "lucide-react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
   return (
@@ -13,6 +14,46 @@ const ProductCard = ({ product }: { product: ProductType }) => {
             fill
             className="object-cover hover:scale-105 transition-all duration-500"
           />
+        </div>
+        <div className="flex flex-col gap-4 p-4">
+          <h1 className="font-medium"> {product.name} </h1>
+          <p className="text-sm text-gray-500"> {product.description}</p>
+          <div className="flex ittems-center gap-4 text-xs">
+            <div className="flex flex-col gap-1">
+              <span className="text-gray-500">Size</span>
+              <select
+                name="size"
+                id="size"
+                className="ring ring-gray-300 rounded-md px-2 py-1"
+              >
+                {product.sizes.map((size) => (
+                  <option key={size} value={size}>
+                    {size}
+                  </option>
+                ))}
+              </select>
+              <div className="flex flex-col gap-1">
+                <span className="text-gray-500">Colors</span>
+                <div className="flex items-center gap-2">
+                  {product.colors.map((colors) => (
+                    <div key={colors}>
+                      <div
+                        className="w-[14p] h-[14p] rounded-full"
+                        style={{ backgroundColor: colors }}
+                      ></div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="font-medium">${product.price.toFixed(2)}</p>
+              <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer houver:text-white hover:bg-black transition-all during-300 flex items-center gap-2">
+                <ShoppingCart className="w-4 h-4" />
+                Add ro Cart
+              </button>
+            </div>
+          </div>
         </div>
       </Link>
     </div>
